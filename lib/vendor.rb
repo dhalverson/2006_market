a8b9febf3955bd35bde88069bc0839758e9f396e
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-29 09:02:30 -0600

    Add vendor class and test it exists

---
+++ lib/vendor.rb
@@ -0,0 +1,10 @@
+class Vendor
+
+  def initialize(name)
+    @name = name
+  end
+
+
+
+
+end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -5,8 +5,9 @@ require './lib/vendor'
 class VendorTest < Minitest::Test
 
   def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
 
-
+    assert_instance_of Vendor, vendor
   end
 
 
