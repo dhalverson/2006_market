388e86e18da89b9795aa55372e8946824ad08dbf
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-29 09:14:29 -0600

    Add market name attr reader

--- test/market_test.rb
+++ test/market_test.rb
@@ -12,4 +12,10 @@ class MarketTest < Minitest::Test
     assert_instance_of Market, market
   end
 
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market", market.name
+  end
+
 end
