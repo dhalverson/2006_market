a263cdcd810d625010d3054bed4608c6422232ad
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-29 08:59:56 -0600

    Adds item class and tests

---
+++ test/item_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    assert_instance_of Item, item2
+  end
+
+  def test_it_has_attributes
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    assert_equal "Tomato", item2.name
+    assert_equal 0.5, item2.price
+  end
+end
---
+++ test/vendor_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists
+
+
+  end
+
+
+end
